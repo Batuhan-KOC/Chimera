27ab5f991b2cf7f66836e6ad12bd4756b6c75526
Batuhan-KOC <1997.batuhankoc@gmail.com>
2023-04-25 17:53:59 +0300

    Initial commit on ChString

--- source/ChString/ChString.cpp
+++ source/ChString/ChString.cpp
@@ -4,6 +4,40 @@ ChString::ChString()
 {
 }
 
+ChString::ChString(const std::string &value) : data_(value)
+{
+}
+
+ChString::ChString(const ChString &other) : data_(other.data_)
+{
+}
+
+ChString::ChString(ChString &&other) noexcept : data_(std::move(other.data_))
+{
+}
+
 ChString::~ChString()
 {
+}
+
+ChString &ChString::operator=(const ChString &other)
+{
+    data_ = other.data_;
+    return *this;
+}
+
+ChString &ChString::operator=(ChString &&other) noexcept
+{
+    data_ = std::move(other.data_);
+    return *this;
+}
+
+ChString::operator std::string &()
+{
+    return data_;
+}
+
+ChString::operator const std::string &() const
+{
+    return data_;
 }
--- source/ChString/ChString.h
+++ source/ChString/ChString.h
@@ -1,10 +1,76 @@
 #ifndef CHSTRING
 #define CHSTRING
 
+#include <string>
+
 class ChString
 {
+public:
+    /**
+     * @brief Default constructor. Constructs an empty string.
+     */
     ChString();
+
+    /**
+     * @brief Overloaded constructor. Constructs the string with the copy of the contents of `value`.
+     *
+     * @param value A std::string object to be used as source to initialize the content of the string with.
+     */
+    ChString(const std::string &value);
+
+    /**
+     * @brief Copy constructor. Constructs the string with the copy of the contents of `other`.
+     *
+     * @param other Another ChString object to be used as source to initialize the content of the string with.
+     */
+    ChString(const ChString &other);
+
+    /**
+     * @brief Move constructor. Constructs the string with the contents of `other` using move semantics.
+     *
+     * @param other Another ChString object to be used as source to initialize the content of the string with. `other` is left in a valid but unspecified state.
+     */
+    ChString(ChString &&other) noexcept;
+
+    /**
+     * @brief Destructor. Destroys the string object and frees the allocated memory.
+     */
     ~ChString();
+
+    /**
+     * @brief Copy assignment operator. Replaces the contents of the string with a copy of the contents of `other`.
+     *
+     * @param other Another ChString object to be used as source to copy the data from.
+     *
+     * @return A reference to the string object.
+     */
+    ChString &operator=(const ChString &other);
+
+    /**
+     * @brief Move assignment operator. Replaces the contents of the string with the contents of `other` using move semantics.
+     *
+     * @param other Another ChString object to be used as source to move the data from. `other` is left in a valid but unspecified state.
+     *
+     * @return A reference to the string object.
+     */
+    ChString &operator=(ChString &&other) noexcept;
+
+    /**
+     * @brief Conversion operator to implicitly convert ChVector to std::string.
+     *
+     * @return A reference to the internal std::string object.
+     */
+    operator std::string &();
+
+    /**
+     * @brief Conversion operator to implicitly convert ChVector to std::string.
+     *
+     * @return A const reference to the internal std::string object.
+     */
+    operator const std::string &() const;
+
+private:
+    std::string data_;
 };
 
 #endif
